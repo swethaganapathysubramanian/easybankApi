9e4dbac82a5fb15b90cfcffc6b2e18eb372ad58c
Swetha Ganapathy subramanian <swethaganapathy2@gmail.com>
2020-07-09 14:43:13 -0700

    Backend to store user data to Invite

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const knex = require('knex');
+
+var db = knex ({
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1',
+        user: 'postgres',
+        password: 'test',
+        database: 'easybank'
+    }
+});
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send("Hello");
+})
+
+app.post('/invite', (req, res) => {
+
+    res.send(req.body);
+
+    db('invites').insert({
+        name:req.body.name,
+        email:req.body.email
+    }).then(console.log)
+    .catch(err=>res.status(400).json(err))
+})
+
+app.listen(3000);
